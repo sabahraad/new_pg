e663319ec6f5582e2dc67f292021af46c614458c
Sabah Raad <raad@aamarpay.net>
2023-03-27 12:08:15 +0600

    post method , pay route

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('form', [Aamarpay\PaymentGateway\Controllers\formController::class,'form'])->name('form');
 Route::post('pay',[Aamarpay\PaymentGateway\Controllers\formController::class,'pay'])->name('pay');
-Route::post('success',[Aamarpay\PaymentGateway\Controllers\formController::class,'success'])->name('success');
-Route::post('fail',[Aamarpay\PaymentGateway\Controllers\formController::class,'fail'])->name('fail');
